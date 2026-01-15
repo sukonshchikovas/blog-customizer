91b3b2254e03b9def6811bf98ba85e2f3de5720a
sukonshchikovas <sukonshchikovas@yandex.ru>
2026-01-15 17:11:42 +0300

    исправления

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
+import { StrictMode } from 'react';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import App from './App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [articleSettings, setArticleSettings] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	const handleApplySettings = (settings: ArticleStateType) => {
-		setArticleSettings(settings);
-	};
-
-	const handleResetSettings = () => {
-		setArticleSettings(defaultArticleState);
-	};
-
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': articleSettings.fontFamilyOption.value,
-					'--font-size': articleSettings.fontSizeOption.value,
-					'--font-color': articleSettings.fontColor.value,
-					'--container-width': articleSettings.contentWidth.value,
-					'--bg-color': articleSettings.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm
-				settings={articleSettings}
-				onApply={handleApplySettings}
-				onReset={handleResetSettings}
-			/>
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
